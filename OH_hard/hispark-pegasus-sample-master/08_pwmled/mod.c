b4347aedb4d3c0fa677d5b23da3d132e9ecff086
TxZgX <xcl@xuegao-tzx.top>
2021-11-26 11:18:13 +0800

    add mod1

---
+++ OH_hard/hispark-pegasus-sample-master/08_pwmled/mod.c
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2020, HiHope Community.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "ohos_init.h"
+//#include "cmsis_os2.h"
+//#include "wifiiot_gpio.h"
+//#include "wifiiot_gpio_ex.h"
+//#include "wifiiot_pwm.h"
+//#include "wifiiot_errno.h"
+
+#include "iot_pwm.h"
+#include "iot_gpio.h"
+
+//#define RED_LED_PIN_NAME WIFI_IOT_IO_NAME_GPIO_10
+//#define RED_LED_PIN_FUNCTION WIFI_IOT_IO_FUNC_GPIO_10_GPIO
+
+#define RESOLUTION 4096
+//#define PWM_FREQ_DIVITION 64000
+
+static void PWMLedDemoTask(void *arg)
+{
+    (void)arg;
+    
+    // GpioDeinit(); 
+    ///IoSetFunc(RED_LED_PIN_NAME, WIFI_IOT_IO_FUNC_GPIO_10_PWM1_OUT);
+    IoTGpioSetDir(10,1);
+    ///PwmInit(WIFI_IOT_PWM_PORT_PWM1); // R
+    IoTPwmInit(1);
+
+    while (1) {
+        // use PWM control RED LED brightness
+        for (int i = 1; i <= RESOLUTION; i *= 2) {
+            ///PwmStart(WIFI_IOT_PWM_PORT_PWM1, i, PWM_FREQ_DIVITION);
+            IoTPwmStart(1,i,64000);
+            usleep(250000);
+            ///PwmStop(WIFI_IOT_PWM_PORT_PWM1);
+            IoTPwmStop(1);
+        }
+    }
+}
+
+static void PWMLedDemo(void)
+{
+    osThreadAttr_t attr;
+
+    ///GpioInit();
+    IoTGpioInit();
+    // set Red/Green/Blue LED pin to GPIO function
+    ///IoSetFunc(RED_LED_PIN_NAME, RED_LED_PIN_FUNCTION);
+    IoTGpioSetDir(10,0);
+
+    attr.name = "PWMLedDemoTask";
+    attr.attr_bits = 0U;
+    attr.cb_mem = NULL;
+    attr.cb_size = 0U;
+    attr.stack_mem = NULL;
+    attr.stack_size = 4096;
+    attr.priority = osPriorityNormal;
+
+    if (osThreadNew(PWMLedDemoTask, NULL, &attr) == NULL) {
+        printf("[ColorfulLightDemo] Falied to create PWMLedDemoTask!\n");
+    }
+}
+
+APP_FEATURE_INIT(PWMLedDemo);
